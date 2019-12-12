0d0cbeaf0572e56924d2d665e254e9e7f32c53f8
Lilibeth Santo <itzlilibeth@gmail.com>
2019-12-12 06:30:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,10 @@
-def valid_position?(board, position)
- if position.to_i.between?(1,9) 
-    if !position_taken?(board, position.to_i-1)
-      true
-    end
-end
+# def valid_position?(board, position)
+# if position.to_i.between?(1,9) 
+#     if !position_taken?(board, position.to_i-1)
+#       true
+#     end
+# end
  
- def position_taken?(board, location)
-  !(position(board, location).nil? || position(board, location) == " ")
-end
+# def position_taken?(board, location)
+#   !(position(board, location).nil? || position(board, location) == " ")
+# end
